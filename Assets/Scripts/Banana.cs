d89c817acb59f6e4fc1b82d665959982b5ba2a2d
Samuel Abiodun <samuelabiodun4square@gmail.com>
2022-07-22 13:12:55 +0100

    Base-file

---
+++ Assets/Scripts/Banana.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : MonoBehaviour
+{
+    [SerializeField] AudioSource source;
+    [SerializeField] private Transform mount;
+
+    private Vector2 originalPos;
+
+
+    //calculate offset between center of gameobject and touch position
+    private float deltaX, deltaY;
+
+    //true when gameobject is dropped in correct place
+    public static bool locked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Banana to appear in original spawn point
+        originalPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPoint = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                //when screen is just touched
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPoint))//if banana is touched calculate offset between touc position and center of bear game object for smooth dragging
+                    {
+                        deltaX = touchPoint.x - transform.position.x;
+                        deltaY = touchPoint.y - transform.position.y;
+                    }
+                    break;
+
+                //if moving finger on the screen, banana position will be set to touch position accounting for offset
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPoint))
+                    {
+                        transform.position = new Vector2(touchPoint.x - deltaX, touchPoint.y - deltaY);
+                    }
+                    break;
+
+                //if finger has been released, check if it's in the right place(or close enough). If it isn't, return to original position
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - mount.position.x) <= 0.5f && Mathf.Abs(transform.position.y - mount.position.y) <= 0.5f)
+                    {
+                        transform.position = new Vector2(mount.position.x, mount.position.y);
+                        locked = true;
+                    }
+                    else
+                    {
+                        transform.position = new Vector2(originalPos.x, originalPos.y);
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] AudioSource source;
+    [SerializeField] AudioClip appear;
+    [SerializeField] AudioClip success;
+
+    [SerializeField] private List<GameObject> fruitPrefabs;
+
+    [SerializeField] private Transform spawnPoint;
+
+    [SerializeField] private GameObject first, second, third;
+
+    public GameObject text;
+
+    bool not1, not2, not3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        not1= true;
+        not2 = true;
+        not3 = true;
+        text.SetActive(false);
+        Invoke("Beginning", 1.5f);
+        //var randomSet = fruitPrefabs.OrderBy(s => Random.value).Take(3).ToList();
+        /*for (int i = 0; i < randomSet.Count; i++)
+        {
+            var spawnPlayer = Instantiate(randomSet[i], spawnPoint.position, Quaternion.identity);
+        }*/
+        //var spawner = Instantiate(randomSet[0], spawnPoint.position, Quaternion.identity);
+ 
+        //StartCoroutine(Spawn(2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var randomSet = fruitPrefabs.OrderBy(s => Random.value).Take(3).ToList();
+        if (Banana.locked && not1)
+        {
+            source.PlayOneShot(success);
+            source.PlayOneShot(appear);
+            second.SetActive(true);
+            not1 = false;
+        }
+
+        if (Grape.locked && not2)
+        {
+            source.PlayOneShot(success);
+            source.PlayOneShot(appear);
+            third.SetActive(true);
+            not2 = false;
+        }
+
+        if (Cherry.locked && not3)
+        {
+            source.PlayOneShot(success);
+            not3 = false;
+        }
+        if (Grape.locked && Cherry.locked && Grape.locked)
+        {
+            text.SetActive(true);
+        }
+    }
+
+    void Beginning()
+    {
+        source.PlayOneShot(appear);
+        first.SetActive(true);
+    }
+
+    /*public IEnumerator Spawn(float time)
+    {
+        int x = 0, lastX = 0;
+        for (int i = 0; i < fruitPrefabs.Count; i++)
+        {
+            while (x == lastX)
+            {
+                x = Random.Range(0, fruitPrefabs.Count);
+            }
+            lastX = x;
+
+            Instantiate(fruitPrefabs[x], spawnPoint.position, Quaternion.identity);
+            yield return new WaitForSeconds(2);
+        }
+    }*/
+}
